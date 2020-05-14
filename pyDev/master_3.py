18eee7723a171e885fd9edc82ccf56d55a495677
joelun37 <aytunc.un@gmail.com>
2020-05-14 12:52:37 +0200

    20200514-1252

--- pyDev/master_3.py
+++ pyDev/master_3.py
@@ -53,80 +53,18 @@ model = BertModel.from_pretrained('bert-base-uncased')
 # Import data
 nq_train_txt_short = pyData + "tensorflow2-question-answering/simplified-nq-train-short.json"
 
-# Tokenize all of the sentences and map the tokens to thier word IDs.
-input_ids = []
-attention_masks = []
+dict_obs = {}
+with open(nq_train_txt_short, 'r') as f:
+    for line in f:
+        obs = json.loads(line)
+        dict_obs[obs["example_id"]] = obs
 
-# For every sentence...
-for sent in sentences:
-    # `encode_plus` will:
-    #   (1) Tokenize the sentence.
-    #   (2) Prepend the `[CLS]` token to the start.
-    #   (3) Append the `[SEP]` token to the end.
-    #   (4) Map tokens to their IDs.
-    #   (5) Pad or truncate the sentence to `max_length`
-    #   (6) Create attention masks for [PAD] tokens.
-    encoded_dict = tokenizer.encode_plus(
-                        sent,                      # Sentence to encode.
-                        add_special_tokens = True, # Add '[CLS]' and '[SEP]'
-                        max_length = 64,           # Pad & truncate all sentences.
-                        pad_to_max_length = True,
-                        return_attention_mask = True,   # Construct attn. masks.
-                        return_tensors = 'pt',     # Return pytorch tensors.
-                   )
-    
-    # Add the encoded sentence to the list.    
-    input_ids.append(encoded_dict['input_ids'])
-    
-    # And its attention mask (simply differentiates padding from non-padding).
-    attention_masks.append(encoded_dict['attention_mask'])
-
-# Convert the lists into tensors.
-input_ids = torch.cat(input_ids, dim=0)
-attention_masks = torch.cat(attention_masks, dim=0)
-labels = torch.tensor(labels)
-
-# Print sentence 0, now as a list of IDs.
-print('Original: ', sentences[0])
-print('Token IDs:', input_ids[0])
-
-
-parts_dict = {}
-window_size = 
-step_size = 128
-example_id = 1
-
-sentence = "Sheets of empty canvas untouched sheets of clay Were laid spread out before me as her body once did All five horizons revolved around her soul as the earth to the sun Now the air I tasted and breathed has taken a turn ooh And all I taught her was everything Ooh I know she gave me all that she wore And now my bitter hands chafe beneath the clouds Of what was everything Oh, the pictures have all been washed in black Tattooed everything Give me fuel give me fire"
-
-sentence=obs["document_text"]
-lst_sentence = sentence.split()
-parts_dict[obs["example_id"]] = []
-for i in range(len(lst_sentence)):
-    if  ((window_size+(i*step_size) - 1) < len(lst_sentence)):
-        part_of_sentence = [lst_sentence[word] for word in range(i*step_size, window_size+(i*step_size))]
-        parts_dict[example_id].append(" ".join(part_of_sentence))
-
-for i, word in enumerate(sentence):
-    for window in range(window_size):
-        if ((i + window + 1) < len(sentence)):
-            my_dict[word].append(sentence[i + window + 1])
-        if ((i - window -1) > -1):
-            my_dict[word].append(sentence[i - window - 1])
-
-for i in range(len(parts_dict[-5739403430449762964])):
-    print("\n")
-    print(parts_dict[-5739403430449762964][i])
-
+for i,k in enumerate(dict_obs):
+    print(dict_obs[k]["question_text"])
 
 parts_dict = {}
 window_size = 509 - len(obs["question_text"])
 step_size = 128
-# example_id = 1
-
-input_ids = []
-
-len(obs["question_text"])
-
 
 """
 The loop below contains a minor bug. It does not take into account the last window
@@ -165,12 +103,97 @@ input_ids.append(encoded_dict["input_ids"])
 for i in range(len(input_ids)):
     print(input_ids[i].shape[1])
 
+# Transform dictionary of observations
+transformed_dict_obs = {}
+parts_dict = {}
+for key in dict_obs:
+    transformed_dict_obs[key]={}
+    obs = dict_obs[key]
+    sentence=obs["document_text"]
+    lst_sentence = sentence.split()
+    parts_dict[obs["example_id"]] = {}
+    parts_dict[obs["example_id"]]["question_text"] = obs["question_text"]
+    parts_dict[obs["example_id"]]["passages"] = []
+    parts_dict[obs["example_id"]]["answer_start_ix"] = []
+    parts_dict[obs["example_id"]]["answer_end_ix"] = []
+    la_start = dict_obs[key]["annotations"][0]["long_answer"]["start_token"]
+    la_end = dict_obs[key]["annotations"][0]["long_answer"]["end_token"]
+    parts_dict[obs["example_id"]]["long_answer"] = lst_sentence[la_start:la_end]
+    for i in range(len(lst_sentence)):
+        if  ((window_size+(i*step_size) - 1) < len(lst_sentence)):
+            part_of_sentence = [lst_sentence[word] for word in range(i*step_size, window_size+(i*step_size))]
+            parts_dict[obs["example_id"]]["passages"].append(" ".join(part_of_sentence))
+            if (i*step_size <= la_start) and (window_size+(i*step_size) >= la_end):
+                start_ix = (la_start - i*step_size)
+                end_ix = (la_end - i*step_size)
+            else:
+                start_ix = 0
+                end_ix = 0
+            parts_dict[obs["example_id"]]["answer_start_ix"].append(start_ix)
+            parts_dict[obs["example_id"]]["answer_end_ix"].append(end_ix)
+
+    for k in parts_dict:
+        print("\n")
+        print(k)
+        print("\n")
+        for i in range(len(parts_dict[k]["answer_start_ix"])):
+            if parts_dict[k]["answer_start_ix"][i] != 0:
+                start = parts_dict[k]["answer_start_ix"][i]
+                end = parts_dict[k]["answer_end_ix"][i]
+                print('\n')
+                print(parts_dict[k]["question_text"])
+                print('\n')
+                print(parts_dict[k]["passages"][i])
+                print('\n')
+                print(parts_dict[k]["passages"][i].split()[start:end])
+                print('\n')
+                print(parts_dict[k]["long_answer"])
+
+    input_ids = []
+
+    for i in range(len(parts_dict[obs["example_id"]])):
+        encoded_dict = tokenizer.encode_plus(obs["question_text"],
+                            parts_dict[obs["example_id"]][i],                      # Sentence to encode.
+                            add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+                            max_length = 520,           # Pad & truncate all sentences.
+                            pad_to_max_length = True,
+                            # return_attention_mask = True,   # Construct attn. masks.
+                            return_tensors = 'pt'     # Return pytorch tensors.
+                    )
+        input_ids.append(encoded_dict["input_ids"])
+    transformed_dict_obs[key]["input_ids"] = input_ids
+    transformed_dict_obs[key]["question_text"] = obs["question_text"]
+    transformed_dict_obs[key]["annotations"] = obs["annotations"]
+
+    la_start = transformed_dict_obs[key]["annotations"][0]["long_answer"]["start_token"]
+    la_end = transformed_dict_obs[key]["annotations"][0]["long_answer"]["end_token"]
+    long_answer = []
+    for word_ix in range(la_start, la_end):
+        long_answer.append(lst_sentence[word_ix])
+    transformed_dict_obs[key]["long_answer"] = long_answer
+    # transformed_dict_obs[key]["input_ids"] = input_ids
+    # transformed_dict_obs[key]["question_text"] = obs["question_text"]
+    # transformed_dict_obs[key]["annotations"] = obs["annotations"]
+
+for key in transformed_dict_obs.keys():
+    print(transformed_dict_obs[key]["question_text"])
+    print(transformed_dict_obs[key]["long_answer"])
+
 input_ids[1].shape[1]
 
 # add start and end token spots to the dictionary
 # then try BertQuestionsAnswerin
 # Then try adding the third parameter
 # Hopefully, it'll suffice to add self.param maybe add to the output as well
+
+# Convert the lists into tensors.
+input_ids = torch.cat(input_ids, dim=0)
+attention_masks = torch.cat(attention_masks, dim=0)
+labels = torch.tensor(labels)
+
+# Print sentence 0, now as a list of IDs.
+print('Original: ', sentences[0])
+print('Token IDs:', input_ids[0])
 
 sep_index = input_ids.tolist().index(tokenizer.sep_token_id)
 
